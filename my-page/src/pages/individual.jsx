63ed5e6f86c2a51e3324d871be4484210f21bf79
Mans99 <mans.englund@live.se>
2023-12-02 16:10:20 +0100

    done

--- my-page/src/pages/individual.jsx
+++ my-page/src/pages/individual.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faGitlab, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom';
 
 export default function individual() {
     const location = useLocation();
@@ -31,29 +33,20 @@ export default function individual() {
         }
     }
 
-
-
-
-
-
     return (
-        <div className='flex flex-col items-center h-1/1'>
-            <h1 className='text-fourth text-center md:w-3/4 w-full lg:text-8xl md:text-7xl sm:text-6xl text-5xl leading-normal mt-10'>{location.state.name}</h1>
+        <>
+        <Link className='text-fourth/40 items-center cursor-pointer hover:text-fourth ml-16 md:mt-4 md:text-5xl text-4xl' to={'/projects' }><FontAwesomeIcon icon={faArrowLeft}/></Link>
+       <div className='flex flex-col items-center h-1/1'>
+            <h1 className='text-fourth text-center md:w-3/4 w-full lg:text-8xl md:text-7xl sm:text-6xl text-5xl leading-normal'>{location.state.name}</h1>
             <div className='text-xl text-fourth/40 m-5'>{line()}</div>
             <div className='flex md:flex-row flex-col items-center py-10 w-5/6 h-fit'>
-                <div className='grid lg:grid-cols-3 grid-cols-2 gap-5 items-center w-full h-fit'>
-                    <div className='text-center'>
-                        <p className='lg:text-5xl md:text-3xl text-2xl text-third'>Technologies</p>
-                        <p className='text-fourth md:text-2xl text-lg sm:mt-2'>{location.state.technologies.join(", ")}</p>
-                    </div>
-                    <div className='text-center'>
-                        <p className='lg:text-5xl md:text-3xl text-2xl text-third'>Language</p>
-                        <p className='text-fourth md:text-2xl text-lg sm:mt-2'>{ } hello</p>
-                    </div>
-                    <div className='text-center'>
-                        <p className='lg:text-5xl md:text-3xl text-2xl text-third'>Placeholder</p>
-                        <p className='text-fourth md:text-2xl text-lg sm:mt-2'>{ }hello </p>
-                    </div>
+                <div className={`grid ${Object.keys(location.state.technologies).length % 2 == 0 ? '' : 'lg:grid-cols-3'} grid-cols-2 gap-5 items-center w-full h-fit`}>
+                    {Object.keys(location.state.technologies).map((tech) =>
+                        <div className='text-center'>
+                            <p className='lg:text-5xl md:text-3xl text-2xl text-third'>{tech}</p>
+                            <p className='text-fourth md:text-2xl text-lg sm:mt-2'>{location.state.technologies[tech]}</p>
+                        </div>
+                    )}
 
                 </div>
             </div>
@@ -67,10 +60,11 @@ export default function individual() {
                 <div className='grid md:w-fit w-fit h-fit items-center lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-1'>
                     {location.state.pictures.map((i) =>
                         <div key={i}>
-                            <img src={`${location.state.src}${i}`} alt="" className='h-[350px] w-[250px] m-1 object-cover border border-black/10 shadow-sm rounded-lg'/>
+                            <img src={`${location.state.src}${i}`} alt="" className='h-[350px] w-[250px] m-1 object-cover border border-black/10 shadow-sm rounded-lg' />
                         </div>)}
                 </div>
             </div>
         </div>
+        </>
     )
 }
